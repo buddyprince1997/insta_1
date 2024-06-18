f74c781a324decf681344907693eee0255e04c00
unknown <lotusvnsmahadeva@gmail.com>
2024-06-18 12:15:37 +0530

    added vercel.json file for

--- app.js
+++ app.js
@@ -25,20 +25,20 @@ app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
 app.use(require('./routes/user'))
   
-app.get("/",(req,res)=>{
-    app.use(express.static(path.resolve(__dirname,"client","build")));
-     res.sendFile(path.resolve(__dirname,"client","build","index.html"));
-});
+// app.get("/",(req,res)=>{
+//     app.use(express.static(path.resolve(__dirname,"client","build")));
+//      res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+// });
 
 
 
-// if(process.env.NODE_ENV=="production"){
-//     app.use(express.static('client/build'))
-//     const path = require('path')
-//     app.get("*",(req,res)=>{
-//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-//     })
-// }
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 app.listen(PORT,()=>{
     console.log("server is running on",PORT)
